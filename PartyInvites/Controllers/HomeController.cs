81a8b0c209731d36244a924f2f6aa8d4b35fc329
Baby face dev <babyfacedeveloper@gmail.com>
2021-06-26 22:39:29 +0700

    Done chapter 3

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -13,6 +14,29 @@ namespace PartyInvites.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+
+            }
+            return View();
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
